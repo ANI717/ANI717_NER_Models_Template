fe645fe4a291765317e8a92dd1bb41fd74cef105
Animesh Bala Ani <animesh.ani@live.com>
2022-08-08 02:25:44 -0400

    Serialize Entities

--- src/main.py
+++ src/main.py
@@ -6,12 +6,14 @@
 
 #___Import Modules:
 import os
+import pickle
 
 from regex_model.model_for_spreadsheet.extract_entities import extract_entities
 
 
 #___Global Variables:
 SOURCE_DIRECTORY = "../data/spreadsheets/"
+RESULT_FILE = "../data/results/spreadsheets_entities.pickle"
 
 
 #___List Spreadsheets:
@@ -26,6 +28,16 @@ for spreadsheet in spreadsheets:
     pass
 
 
-#___Print Entities
+#___Print Entities:
 for item in items:
     print(item.get("entities").get("date").get("postprocessed_content"))
+
+
+#___Serialize Entities:
+with open(RESULT_FILE, 'wb') as handle:
+    pickle.dump(items, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+#___Unserialize Entities from Pickle:
+with open(RESULT_FILE, 'rb') as handle:
+    unserialized_items = pickle.load(handle)
